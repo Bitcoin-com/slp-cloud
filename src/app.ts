fa6cc280990c8c62e9b2ab3622c42fd69d712bf6
Gabriel Cardona <gabriel@bitcoin.com>
2019-10-02 13:35:38 -0700

    port.

--- src/app.ts
+++ src/app.ts
@@ -145,7 +145,7 @@ app.use(
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || "3000")
+const port = normalizePort(process.env.PORT || "3001")
 app.set("port", port)
 console.log(`slp-rest started on port ${port}`)
 
